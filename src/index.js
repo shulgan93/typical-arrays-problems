fc417fc78d0c4d5e78ca16f1bbbf4bda3a2a7c11
shulgan93 <mr.shulhan93@gmail.com>
2021-03-05 12:39:01 +0300

    feat: ready task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
+exports.min = function min(array = []) {
+    if (array.length === 0) {
+        return 0;
+    }
 
-exports.min = function min (array) {
-  return 0;
-}
+    return Math.min(...array);
+};
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.max = function max(array = []) {
+    if (array.length === 0) {
+        return 0;
+    }
 
-exports.avg = function avg (array) {
-  return 0;
-}
+    return Math.max(...array);
+};
+
+exports.avg = function avg(array = []) {
+    if (array.length === 0) {
+        return 0;
+    }
+
+    let avg =
+        array.reduce((acc, el) => {
+            return acc + el;
+        }, 0) / array.length;
+
+    return avg;
+};
